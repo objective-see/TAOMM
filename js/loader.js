d48aaa99ef59d8b7a75a78274071f06d0ecec72f
Patrick Wardle <code@objective-see.com>
2022-05-17 12:58:33 -1000

    updates

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,14 @@
 if(document.referrer.indexOf("taomm.org") == -1) {
 
-	$(document).ajaxStart(function() {
+    $(document).ajaxStart(function() {
         $("#loading").show();
+        $('#loading').css('visibility','visible');
     });
 
     $(document).ajaxStop(function() {
         $("#loader").delay(700).fadeOut(100)
         $("#loading").delay(700).fadeOut(500)
     });
+    
 }
   
